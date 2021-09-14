46b1ee7b963079d40e3a3945c0c3ed5d05d10523
sergio1008 <sergiobernalc@outlook.com>
2021-09-14 16:29:51 -0500

    2 files modified

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -25,5 +25,12 @@ var superHeroes: Array<SuperHero> =  [
     franquicia: 'DC comics',
     img: 'https://1.bp.blogspot.com/-8p_mRPosMno/YR_OsFMzTJI/AAAAAAABM_0/QWdNXoESYjkqTFvqhX5ohaJeVwp5gXh3gCLcBGAsYHQ/s1400/Superman-Superboy-Jon-Kent-feature.jpg',
     logo: 'https://logos-marcas.com/wp-content/uploads/2020/11/Superman-Logo.png'
-  },
+  },  {
+    name : 'Batman',
+    description: 'Batman es un personaje de cómic creado por los estadounidenses Bob Kane y Bill Finger, y propiedad de DC Comics. Apareció por primera vez en la historia titulada «El caso del sindicato químico» de la revista Detective Comics N.º 27, lanzada por la editorial National Publications el 30 de marzo de 1939.',
+    ano: 1933,
+    franquicia: 'DC comics',
+    img: 'https://www.diariovivo.com/wp-content/uploads/2019/09/batman-dc-750x450.jpg',
+    logo: 'https://1000marcas.net/wp-content/uploads/2019/11/Logo-Batman-.png'
+  }
 ]
