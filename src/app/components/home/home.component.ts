256185d760f04d9954f2bd3a0bf3edf248f44558
sergio1008 <sergiobernalc@outlook.com>
2021-09-14 16:00:09 -0500

    3 files modified, 1 added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SuperHero } from '../../interfaces/SuperHero';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  heroes: Array<SuperHero> = superHeroes;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+
+var superHeroes: Array<SuperHero> =  [
+  {
+    name : 'Superman',
+    description: 'Superman es un superhéroe ficticio que apareció por primera vez en los cómics estadounidenses publicados por DC Comics. El personaje fue creado por el escritor estadounidense Jerry Siegel y el artista canadiense Joe Shuster en 1933 cuando ambos se encontraban viviendo en Cleveland, Ohio.',
+    ano: 1933,
+    franquicia: 'DC comics',
+    img: 'https://1.bp.blogspot.com/-8p_mRPosMno/YR_OsFMzTJI/AAAAAAABM_0/QWdNXoESYjkqTFvqhX5ohaJeVwp5gXh3gCLcBGAsYHQ/s1400/Superman-Superboy-Jon-Kent-feature.jpg',
+    logo: 'https://logos-marcas.com/wp-content/uploads/2020/11/Superman-Logo.png'
+  },
+]
---
+++ src/app/interfaces/SuperHero.ts
@@ -0,0 +1,8 @@
+export interface SuperHero{
+  name: string;
+  img: string;
+  description: string;
+  franquicia: string;
+  logo: string;
+  ano: number;
+}
