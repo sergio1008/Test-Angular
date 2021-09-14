532088c8221bd9a1b617778ae1701e0ebc455a87
sergio1008 <sergiobernalc@outlook.com>
2021-09-14 16:43:27 -0500

    2 files modified, 1 added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SuperHero } from '../../interfaces/SuperHero';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-home',
@@ -7,30 +8,12 @@ import { SuperHero } from '../../interfaces/SuperHero';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  heroes: Array<SuperHero> = superHeroes;
-  constructor() { }
+  heroes: Array<SuperHero>;
+  constructor(private heroService: HeroService ) {}
 
   ngOnInit() {
+    this.heroes = this.heroService.getHeroes();
   }
-
 }
 
 
-
-var superHeroes: Array<SuperHero> =  [
-  {
-    name : 'Superman',
-    description: 'Superman es un superhéroe ficticio que apareció por primera vez en los cómics estadounidenses publicados por DC Comics. El personaje fue creado por el escritor estadounidense Jerry Siegel y el artista canadiense Joe Shuster en 1933 cuando ambos se encontraban viviendo en Cleveland, Ohio.',
-    ano: 1933,
-    franquicia: 'DC comics',
-    img: 'https://1.bp.blogspot.com/-8p_mRPosMno/YR_OsFMzTJI/AAAAAAABM_0/QWdNXoESYjkqTFvqhX5ohaJeVwp5gXh3gCLcBGAsYHQ/s1400/Superman-Superboy-Jon-Kent-feature.jpg',
-    logo: 'https://logos-marcas.com/wp-content/uploads/2020/11/Superman-Logo.png'
-  },  {
-    name : 'Batman',
-    description: 'Batman es un personaje de cómic creado por los estadounidenses Bob Kane y Bill Finger, y propiedad de DC Comics. Apareció por primera vez en la historia titulada «El caso del sindicato químico» de la revista Detective Comics N.º 27, lanzada por la editorial National Publications el 30 de marzo de 1939.',
-    ano: 1933,
-    franquicia: 'DC comics',
-    img: 'https://www.diariovivo.com/wp-content/uploads/2019/09/batman-dc-750x450.jpg',
-    logo: 'https://1000marcas.net/wp-content/uploads/2019/11/Logo-Batman-.png'
-  }
-]
---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { SuperHero } from '../interfaces/SuperHero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Array<SuperHero> {
+    return superHeroes;
+  }
+}
+
+var superHeroes: Array<SuperHero> = [
+  {
+    name: 'Superman',
+    description:
+      'Superman es un superhéroe ficticio que apareció por primera vez en los cómics estadounidenses publicados por DC Comics. El personaje fue creado por el escritor estadounidense Jerry Siegel y el artista canadiense Joe Shuster en 1933 cuando ambos se encontraban viviendo en Cleveland, Ohio.',
+    ano: 1933,
+    franquicia: 'DC comics',
+    img:
+      'https://1.bp.blogspot.com/-8p_mRPosMno/YR_OsFMzTJI/AAAAAAABM_0/QWdNXoESYjkqTFvqhX5ohaJeVwp5gXh3gCLcBGAsYHQ/s1400/Superman-Superboy-Jon-Kent-feature.jpg',
+    logo:
+      'https://logos-marcas.com/wp-content/uploads/2020/11/Superman-Logo.png'
+  },
+  {
+    name: 'Batman',
+    description:
+      'Batman es un personaje de cómic creado por los estadounidenses Bob Kane y Bill Finger, y propiedad de DC Comics. Apareció por primera vez en la historia titulada «El caso del sindicato químico» de la revista Detective Comics N.º 27, lanzada por la editorial National Publications el 30 de marzo de 1939.',
+    ano: 1939,
+    franquicia: 'DC comics',
+    img:
+      'https://www.diariovivo.com/wp-content/uploads/2019/09/batman-dc-750x450.jpg',
+    logo: 'https://1000marcas.net/wp-content/uploads/2019/11/Logo-Batman-.png'
+  },
+  {
+    name: 'Linterna Verde',
+    description:
+      'Linterna Verde es el alias de varios superhéroes de la ficción del Universo DC, de la editorial DC Comics, los cuales se caracterizan por portar un anillo de poder y tener la capacidad de crear manifestaciones de luz sólida con dichos anillos.',
+    ano: 1940,
+    franquicia: 'DC comics',
+    img: 'https://img.ecartelera.com/noticias/fotos/62500/62594/1.jpg',
+    logo:
+      'https://w7.pngwing.com/pngs/642/930/png-transparent-green-lantern-logo-green-lantern-corps-hal-jordan-superman-logo-green-lantern-logo-comics-cdr-lantern.png'
+  }
+];
