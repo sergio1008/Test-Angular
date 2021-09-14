da963591486f2b73976429099fcb5667d843fae0
sergio1008 <sergiobernalc@outlook.com>
2021-09-14 16:55:37 -0500

    3 files modified

--- src/app/interfaces/SuperHero.ts
+++ src/app/interfaces/SuperHero.ts
@@ -1,4 +1,5 @@
 export interface SuperHero{
+  id: number;
   name: string;
   img: string;
   description: string;
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -12,6 +12,7 @@ export class HeroService {
 
 var superHeroes: Array<SuperHero> = [
   {
+    id: 1,
     name: 'Superman',
     description:
       'Superman es un superhéroe ficticio que apareció por primera vez en los cómics estadounidenses publicados por DC Comics. El personaje fue creado por el escritor estadounidense Jerry Siegel y el artista canadiense Joe Shuster en 1933 cuando ambos se encontraban viviendo en Cleveland, Ohio.',
@@ -23,6 +24,7 @@ var superHeroes: Array<SuperHero> = [
       'https://logos-marcas.com/wp-content/uploads/2020/11/Superman-Logo.png'
   },
   {
+    id: 2,
     name: 'Batman',
     description:
       'Batman es un personaje de cómic creado por los estadounidenses Bob Kane y Bill Finger, y propiedad de DC Comics. Apareció por primera vez en la historia titulada «El caso del sindicato químico» de la revista Detective Comics N.º 27, lanzada por la editorial National Publications el 30 de marzo de 1939.',
@@ -33,12 +35,35 @@ var superHeroes: Array<SuperHero> = [
     logo: 'https://1000marcas.net/wp-content/uploads/2019/11/Logo-Batman-.png'
   },
   {
+    id: 3,
     name: 'Linterna Verde',
     description:
       'Linterna Verde es el alias de varios superhéroes de la ficción del Universo DC, de la editorial DC Comics, los cuales se caracterizan por portar un anillo de poder y tener la capacidad de crear manifestaciones de luz sólida con dichos anillos.',
     ano: 1940,
     franquicia: 'DC comics',
     img: 'https://img.ecartelera.com/noticias/fotos/62500/62594/1.jpg',
+    logo:
+      'https://w7.pngwing.com/pngs/642/930/png-transparent-green-lantern-logo-green-lantern-corps-hal-jordan-superman-logo-green-lantern-logo-comics-cdr-lantern.png'
+  },
+  {
+    id: 3,
+    name: 'Linterna Verde',
+    description:
+      'Linterna Verde es el alias de varios superhéroes de la ficción del Universo DC, de la editorial DC Comics, los cuales se caracterizan por portar un anillo de poder y tener la capacidad de crear manifestaciones de luz sólida con dichos anillos.',
+    ano: 1940,
+    franquicia: 'DC comics',
+    img: 'https://img.ecartelera.com/noticias/fotos/62500/62594/1.jpg',
+    logo:
+      'https://w7.pngwing.com/pngs/642/930/png-transparent-green-lantern-logo-green-lantern-corps-hal-jordan-superman-logo-green-lantern-logo-comics-cdr-lantern.png'
+  },
+  {
+    id: 4,
+    name: 'Aquaman',
+    description:
+      'Aquaman es un superhéroe que aparece en los cómics estadounidenses publicados por DC Comics. Creado por el artista Paul Norris y el escritor Mort Weisinger, el personaje debutó en More Fun Comics # 73 (noviembre de 1941)',
+    ano: 1941,
+    franquicia: 'DC comics',
+    img: 'https://www.yocomics.com/wp-content/uploads/aquaman-acaba-de-robar-un-arma-antigua.jpg',
     logo:
       'https://w7.pngwing.com/pngs/642/930/png-transparent-green-lantern-logo-green-lantern-corps-hal-jordan-superman-logo-green-lantern-logo-comics-cdr-lantern.png'
   }
