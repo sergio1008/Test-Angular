4163f21a95ab4cb5e51ef6871c3c942d714475f2
sergio1008 <sergiobernalc@outlook.com>
2021-09-21 16:45:50 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +13,7 @@ import { HeroDetailComponent } from './components/hero-detail/hero-detail.compon
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, FormsModule, ReactiveFormsModule ],
   declarations: [ 
     AppComponent,
     NavComponent, 
